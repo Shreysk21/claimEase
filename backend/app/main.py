0cd7c711fe775b656bff43726ee597847676da45
ShreySK <sskale_b24@it.vjti.ac.in>
2026-04-24 01:31:26 +0530

    ..

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 # CORS — allow frontend dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["*"],
+    allow_origins     = ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173","https://claim-ease-git-main-shreya-kales-projects.vercel.app"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
